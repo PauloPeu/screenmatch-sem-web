c0f6336361b4033d0d9e8a7cf84410b32ea6c87b
PauloPeu <pcnbrito@hotmail.com>
2025-07-06 22:13:31 -0300

    Alterações na run, criação da classe Principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,34 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9101b678");
+
+//		var consumoApi = new ConsumoAPI();
+//		//modelando séries
+//		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=heroes&apikey=9101b678");
+//		System.out.println(json);
+//		ConverteDados conversor = new ConverteDados();
+//		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+//		System.out.println(dados);
+//
+//		// modelando episódios
+//		json = consumoApi.obterDados("https://omdbapi.com/?t=heroes&season=1&episode=2&apikey=6585022c");
+//		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+//		System.out.println(dadosEpisodio);
+
+//		// modelando temporadas
+//		List<DadosTemporadas> temporadas= new ArrayList<>();
+//		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+//			json = consumoApi.obterDados("https://omdbapi.com/?t=heroes&season=" + i + "&apikey=6585022c");
+//			DadosTemporadas dadosTemporadas = conversor.obterDados(json, DadosTemporadas.class);
+//			temporadas.add(dadosTemporadas);
+//		}
+//		temporadas.forEach(System.out::println);
+
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+		//Exemplo com dados genéricos
 		//System.out.println(json);
 		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
 	}
 }
